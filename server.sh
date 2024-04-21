7851ec1d46f084a069dba4a83131e4feacf5f14c
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2024-04-21 17:43:16 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,4 +7,4 @@ CONTAINER_ID=$(hostname)
 echo "Container ID: $CONTAINER_ID"
 
 # Start a simple web server to display the container ID
-echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nContainer ID: $CONTAINER_ID" | nc -l -p 80
+echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nContaiasdasdsadsadasdadner ID: $CONTAINER_ID" | nc -l -p 80
