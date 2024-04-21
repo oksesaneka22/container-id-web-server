2b796706525386e87b7339e16ff1dd16e163dc28
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2024-04-21 17:39:56 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 docker build -t oksesaneka22/container-id-web-server .
-docker login
 docker push oksesaneka22/container-id-web-server
 docker stack deploy -c docker-compose.yml container-id-web-server
